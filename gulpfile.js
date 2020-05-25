232ac0031932df72c621c241627b708c2403cd34
Kuzma Vladislav <vladkuzma99@gmail.com>
2020-05-25 17:46:39 +0300

    Add sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,36 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
 const cache = require('gulp-cache');
 
+function cssStyle(done) {
+    gulp.src('./sass/**/*.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            errorLogToConsole: true,
+            outputStyle: 'expanded'
+        }))
+        .on('error', console.error.bind(console))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('./css/'))
+        .pipe(browserSync.stream());
+    done();
+}
+
+function watchSass() {
+    gulp.watch('./sass/**/*', cssStyle);
+}
+
 function watchFiles() {
-    gulp.watch('./**/*.css', browserReload);
+    gulp.watch('./sass/**/*', cssStyle);
     gulp.watch('./**/*.html', browserReload);
-    gulp.watch('./**/*.php', browserReload);
     gulp.watch('./**/*.js', browserReload);
 }
 
@@ -29,4 +51,4 @@ function browserReload(done) {
 }
 
 gulp.task('default', gulp.parallel(watchFiles, sync));
-gulp.task('files', gulp.parallel(watchFiles));
+gulp.task('sass', gulp.parallel(watchFiles));
